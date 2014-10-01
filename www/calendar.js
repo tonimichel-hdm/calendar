45bee40908aa6ee5064be276c59905f32d51df77
tonimichel-hdm <tm063@hdm-stuttgart.de>
2014-10-01 15:54:03 +0200

    bugfix

--- www/calendar.js
+++ www/calendar.js
@@ -1,19 +1,17 @@
-var calendar = {
-	createEvent: function( title, location, notes, startDate, endDate, successCallback, errorCallback ) 	
-	{
-		cordova.exec(
-			successCallback,
-			errorCallback,
-			'Calendar',
-			'addCalenderEntry',
-			[{
-				"title": title,
-				"description": notes,
-				"eventLocation": location,
-				"startTimeMillis": startDate.getTime(),
-				"endTimeMillis": endDate.getTime()
-			}]
-		);
-	}
+window.calendar = function (title, location, notes, startDate, endDate, successCallback, errorCallback) {
+    
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        'Calendar',
+        'addCalenderEntry',
+        [{
+            "title": title,
+            "description": notes,
+            "eventLocation": location,
+            "startTimeMillis": startDate.getTime(),
+            "endTimeMillis": endDate.getTime()
+        }]
+    );
 }
 module.exports = calendar;
